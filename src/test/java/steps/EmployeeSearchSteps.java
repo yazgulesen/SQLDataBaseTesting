91391719ac226f1debd06545fd99b33f35f4367e
sohai <SyntaxTechnologies>
2022-04-26 05:38:30 +0530

    lc

---
+++ src/test/java/steps/EmployeeSearchSteps.java
@@ -0,0 +1,46 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class EmployeeSearchSteps {
+
+    @Given("user is navigated to HRMS application")
+    public void user_is_navigated_to_hrms_application() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user enters valid admin credentials")
+    public void user_enters_valid_admin_credentials() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user clicks on login button")
+    public void user_clicks_on_login_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user navigated to employee list page")
+    public void user_navigated_to_employee_list_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user enters valid employee id")
+    public void user_enters_valid_employee_id() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user clicks on search button")
+    public void user_clicks_on_search_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user is able to see employee information")
+    public void user_is_able_to_see_employee_information() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+}
