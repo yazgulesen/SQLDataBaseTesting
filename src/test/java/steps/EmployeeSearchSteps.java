0ee7d5f08cece4092944559c594631b56346aaa6
sohai <SyntaxTechnologies>
2022-04-27 04:59:34 +0530

    lc0427

--- src/test/java/steps/EmployeeSearchSteps.java
+++ src/test/java/steps/EmployeeSearchSteps.java
@@ -3,8 +3,14 @@ package steps;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.Command;
 import utils.CommonMethods;
+import utils.ConfigReader;
+import utils.Constants;
+
+import java.util.concurrent.TimeUnit;
 
 public class EmployeeSearchSteps extends CommonMethods {
 
@@ -14,34 +20,43 @@ public class EmployeeSearchSteps extends CommonMethods {
     }
     @When("user enters valid admin credentials")
     public void user_enters_valid_admin_credentials() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        WebElement usernamefield = driver.findElement(By.id("txtUsername"));
+        usernamefield.sendKeys(ConfigReader.getPropertyValue("username"));
+        WebElement passwordField = driver.findElement(By.name("txtPassword"));
+        passwordField.sendKeys(ConfigReader.getPropertyValue("password"));
+
     }
+
     @When("user clicks on login button")
     public void user_clicks_on_login_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        WebElement loginButton = driver.findElement(By.id("btnLogin"));
+        loginButton.click();
+        driver.manage().timeouts().implicitlyWait(Constants.IMPLICIT_WAIT, TimeUnit.SECONDS);
     }
+
     @When("user navigated to employee list page")
     public void user_navigated_to_employee_list_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        WebElement pimOption = driver.findElement(By.id("menu_pim_viewPimModule"));
+        pimOption.click();
+
+        WebElement empListOption = driver.findElement(By.id("menu_pim_viewEmployeeList"));
+        empListOption.click();
     }
+
     @When("user enters valid employee id")
     public void user_enters_valid_employee_id() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        WebElement searchId = driver.findElement(By.id("empsearch_id"));
+        searchId.sendKeys("8510142");
     }
+
     @When("user clicks on search button")
     public void user_clicks_on_search_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        WebElement searchButton = driver.findElement(By.id("searchBtn"));
+        searchButton.click();
     }
+
     @Then("user is able to see employee information")
     public void user_is_able_to_see_employee_information() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Result displayed");
     }
-
-
 }
--- src/test/java/utils/Constants.java
+++ src/test/java/utils/Constants.java
@@ -1,8 +1,11 @@
 package utils;
 
+import com.github.dockerjava.api.model.CpuUsageConfig;
+
 public class Constants {
 
     public static final String CONFIGURATION_FILEPATH = System.getProperty("user.dir")+"/src/test/resources/config/config.properties";
-
+    public static final int IMPLICIT_WAIT = 10;
+    public static final int EXPLICIT_WAIT = 20;
 
 }
