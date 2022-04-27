e37212367dbeba060c9def27f3f6f9d21156175c
sohai <SyntaxTechnologies>
2022-04-27 06:58:06 +0530

    lc0427

--- src/test/java/utils/CommonMethods.java
+++ src/test/java/utils/CommonMethods.java
@@ -1,10 +1,15 @@
 package utils;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.NoSuchFrameException;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 public class CommonMethods {
 
@@ -25,6 +30,40 @@ public class CommonMethods {
                 throw new RuntimeException("Invalid browser name");
         }
         driver.get(ConfigReader.getPropertyValue("url"));
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Constants.IMPLICIT_WAIT, TimeUnit.SECONDS);
+    }
+
+    public static void sendText(WebElement element, String textToSend){
+        element.clear();
+        element.sendKeys(textToSend);
+    }
+
+    public static WebDriverWait getWait(){
+        WebDriverWait wait = new WebDriverWait(driver, Constants.EXPLICIT_WAIT);
+        return wait;
+    }
+
+    public static void waitForClickability(WebElement element){
+        getWait().until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public static void click(WebElement element){
+        waitForClickability(element);
+        element.click();
+    }
+
+    public static JavascriptExecutor getJSExecutor(){
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        return js;
+    }
+
+    public static void jsClick(WebElement element){
+        getJSExecutor().executeScript("arguments[0].click();", element);
+    }
+
+    public static void tearDown(){
+        driver.quit();
     }
 
 }
--- src/test/resources/features/EmployeeSearch.feature
+++ src/test/resources/features/EmployeeSearch.feature
@@ -12,7 +12,6 @@ Feature: US-12345 - Search an employee in HRMS
     Given user is navigated to HRMS application
     When user enters valid admin credentials
     And user clicks on login button
-
     And user navigated to employee list page
     When user enters valid employee name
     And user clicks on search button
