34373d20c4155482d5cb6bf43a6dd441d09c55d0
sohai <SyntaxTechnologies>
2022-05-02 00:05:21 +0530

    lc0501

--- src/test/java/steps/AddEmployeeSteps.java
+++ src/test/java/steps/AddEmployeeSteps.java
@@ -1,10 +1,12 @@
 package steps;
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import utils.CommonMethods;
+
+import java.util.List;
+import java.util.Map;
 
 public class AddEmployeeSteps extends CommonMethods {
 
@@ -47,6 +49,27 @@ public class AddEmployeeSteps extends CommonMethods {
        sendText(addEmployeePage.firstNameField, firstName);
        sendText(addEmployeePage.middleNameField, middleName);
        sendText(addEmployeePage.lastNameField, lastName);
+    }
+
+    @When("user provides multiple employee data and verify they are added")
+    public void user_provides_multiple_employee_data_and_verify_they_are_added(DataTable dataTable) throws InterruptedException {
+           List<Map<String, String>> employeeNames =  dataTable.asMaps();
+        for (Map<String, String> employee :employeeNames) {
+                   System.out.println(employee);
+                   String firstNameValue = employee.get("firstName");
+                   String middleNameValue = employee.get("middleName");
+                   String lastNameValue = employee.get("lastName");
+            System.out.println(firstNameValue + " " + middleNameValue + " " + lastNameValue);
+            sendText(addEmployeePage.firstNameField, firstNameValue);
+            sendText(addEmployeePage.middleNameField, middleNameValue);
+            sendText(addEmployeePage.lastNameField, lastNameValue);
+            click(addEmployeePage.saveButton);
+            Thread.sleep(3000);
+
+            //verification of adding an employee is HW
+            click(employeeSearchPage.addEmployeeOption);
+        }
+
     }
 
 }
