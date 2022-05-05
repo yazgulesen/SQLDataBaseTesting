172fd683fbb5b595b0d681a5be28df9766e928da
sohai <SyntaxTechnologies>
2022-05-05 07:27:55 +0530

    lc0505

--- src/test/java/utils/Constants.java
+++ src/test/java/utils/Constants.java
@@ -7,4 +7,5 @@ public class Constants {
     public static final String CONFIGURATION_FILEPATH = System.getProperty("user.dir")+"/src/test/resources/config/config.properties";
     public static final int IMPLICIT_WAIT = 10;
     public static final int EXPLICIT_WAIT = 20;
+    public static final String TESTDATA_FILEPATH = System.getProperty("user.dir")+"/src/test/resources/testdata/batch12ExcelFile.xlsx";
 }
